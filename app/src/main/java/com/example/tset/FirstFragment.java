b56d420cb6b91443d603dc099a7cb9ba3aba90cd
lizhiyue2019 <1654168745@qq.com>
2022-09-22 00:40:13 +0800

    从 dev2 合并

--- app/src/main/java/com/example/tset/FirstFragment.java
+++ app/src/main/java/com/example/tset/FirstFragment.java
@@ -16,7 +16,7 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
+            Bundle sgiavedInstanceState
     ) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
